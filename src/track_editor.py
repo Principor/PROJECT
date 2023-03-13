b5b23cd78a88bf13f42677a04c6681a5dc3f059a
Christopher Jewszel <c.jewszel@gmail.com>
2023-03-13 15:07:18 +0000

    Set up TKinter

---
+++ src/track_editor.py
@@ -0,0 +1,14 @@
+from tkinter import Tk
+
+
+class TrackEditor:
+    def __init__(self):
+        self.root = Tk()
+        self.root.resizable(False, False)
+        self.root.mainloop()
+
+
+if __name__ == '__main__':
+    TrackEditor()
+
+
