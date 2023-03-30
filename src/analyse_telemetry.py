efbac4dc02362fa1a95039c2670f53cf8ab29ff1
Christopher Jewszel <c.jewszel@gmail.com>
2023-03-31 00:00:16 +0100

    Fixed analyse_telemetry.py

--- src/analyse_telemetry.py
+++ src/analyse_telemetry.py
@@ -9,17 +9,22 @@ import matplotlib.pyplot as plt
 from model import Model, state_to_tensor
 import racecar_driving
 
-RUN_NAME = "ff"
-new_telemetry = True
-plot_throttle = False
-plot_position = False
-plot_speed = True
+RUN_NAME = "lstm_asymmetric"
+CAR_INDEX = 1
+TRACK_NAME = "test"
+NEW_TELEMETRY = True
+PLOT_THROTTLE = False
+PLOT_POSITION = True
+PLOT_SPEED = False
+
 
 def main():
-    if new_telemetry:
-        env = DummyVecEnv([lambda: gym.make('RacecarDriving-v0', save_telemetry=True, random_start=False)])
+    if NEW_TELEMETRY:
+        env = DummyVecEnv([lambda: gym.make('RacecarDriving-v0', save_telemetry=True, random_start=False,
+                                            car_index=CAR_INDEX, track_list=[TRACK_NAME], transform_tracks=False)])
         # Load normaliser generated during training so inputs match
         env = VecNormalize.load("../models/{}/normaliser".format(RUN_NAME), env)
+        env.training = False
         actor = Model.load_model("../models/{}/model.pth".format(RUN_NAME))
 
         done = False
@@ -34,12 +39,16 @@ def main():
         track_width = data["track_width"]
         telemetry = data["telemetry"]
 
+        if telemetry[0][0] + track.num_segments > telemetry[-1][0]:
+            print("Invalid telemetry - 1 lap was not completed!")
+            return
+
         start_index = 1
         while telemetry[start_index][0] == telemetry[0][0]:
             start_index += 1
 
         end_index = len(telemetry) - 1
-        while telemetry[end_index][0] > track.num_segments * 2 - 1:
+        while telemetry[end_index][0] > telemetry[0][0] + track.num_segments:
             end_index -= 1
 
         telemetry = telemetry[start_index:end_index]
@@ -47,14 +56,14 @@ def main():
         progress = np.array([track.get_total_progress(item[0], item[1]) for item in telemetry])
         progress -= np.min(progress)
 
-        if plot_throttle:
+        if PLOT_THROTTLE:
             throttle = np.clip(np.array(telemetry)[:, 4], 0, 1)
             brake = np.clip(-np.array(telemetry)[:, 5], 0, 1)
             plt.plot(progress, throttle, color='g')
             plt.plot(progress, brake, color='r')
             plt.show()
 
-        if plot_position:
+        if PLOT_POSITION:
             left_points = []
             right_points = []
             car_points = []
@@ -68,12 +77,15 @@ def main():
                 right_points.append((mid_point + right * track_width / 2).tuple())
                 car_points.append(car_position.tuple())
 
+            left_points.append(left_points[0])
+            right_points.append(right_points[0])
+
             plt.plot([x for x, _ in left_points], [y for _, y in left_points], color="black")
             plt.plot([x for x, _ in right_points], [y for _, y in right_points], color="black")
             plt.plot([x for x, _ in car_points], [y for _, y in car_points], color="blue")
             plt.show()
 
-        if plot_speed:
+        if PLOT_SPEED:
             plt.plot(progress, np.array(telemetry)[:, 3])
             plt.show()
 
